cc18a05052cb21329aaf2a8dec484fac768aae5d
Nithin Richard <nithin.richard@hodo.in>
2026-01-08 12:09:15 +0530

    fix: resolve 'afterEvaluate' evaluation error and upgrade Kotlin to 2.1.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,18 +12,14 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
     project.evaluationDependsOn(":app")
 }
 
 subprojects {
-    afterEvaluate {
-        val project = this
-        if (project.hasProperty("android")) {
-            val android = project.property("android") as com.android.build.gradle.BaseExtension
+    val subproject = this
+    subproject.plugins.whenPluginAdded {
+        if (this is com.android.build.gradle.AppPlugin || this is com.android.build.gradle.LibraryPlugin) {
+            val android = subproject.extensions.getByType<com.android.build.gradle.BaseExtension>()
             android.compileSdkVersion(35)
             android.defaultConfig {
                 targetSdkVersion(35)
