e0dfd007f21dadd6dc59143796cedccc68b70210
Nithin Richard <nithin.richard@hodo.in>
2026-01-08 11:51:28 +0530

    fix: upgrade compileSdk and targetSdk to 35 to resolve AAR metadata errors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.the_180_project"
-    compileSdk = 34
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -26,7 +26,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21 // Required for ML Kit and Firebase
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true
